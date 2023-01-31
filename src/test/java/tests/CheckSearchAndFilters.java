c139cf7969c2591ec5e384cffb081876d80aead7
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-01-31 21:44:08 +0300

    Added fix remote url

--- src/test/java/tests/CheckSearchAndFilters.java
+++ src/test/java/tests/CheckSearchAndFilters.java
@@ -1,13 +1,6 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
